1ee1e78925298c2fc2e6653985868b7f383cbd5c
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-23 14:26:56 -0600

    Initial Commit

---
+++ OReilleyMVCwKnockout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OReilleyMVCwKnockout.Startup))]
+namespace OReilleyMVCwKnockout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
