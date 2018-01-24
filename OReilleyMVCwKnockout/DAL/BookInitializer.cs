8ba48636a1af09a081a8657b1fcb7e41b427e9ae
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-24 13:45:57 -0600

    Completed Book Initializer

--- OReilleyMVCwKnockout/DAL/BookInitializer.cs
+++ OReilleyMVCwKnockout/DAL/BookInitializer.cs
@@ -20,8 +20,28 @@ namespace OReilleyMVCwKnockout.DAL
 
             var books = new List<Book>
             {
-
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageURL = "http://ecx.images-amazon.com/images/I/51T%2BWt430bl._AA160_.jpg",
+                    ISBN = "1491914319",
+                    Synopsis = "...",
+                    Title = "Knockout.js: Building Dynamic Client-Side Web Applications"                    
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageURL = "http://ecx.images-amazon.com/images/I/51AkFkNeUxl._AA160_.jpg",
+                    ISBN = "1449319548",
+                    Synopsis = "...",
+                    Title = "20 Recipes for Programming PhoneGap: Cross-Platform Mobile Development"
+                }
             };
+            books.ForEach(b => context.Books.Add(b));
+
+            context.SaveChanges();
         }
     }
 }
