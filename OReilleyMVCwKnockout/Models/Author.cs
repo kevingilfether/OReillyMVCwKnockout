9b90956d3a3f9bd600db31410db7be1a541910e0
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-24 13:02:34 -0600

    Started Code-First Book-Author model

---
+++ OReilleyMVCwKnockout/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OReilleyMVCwKnockout.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ OReilleyMVCwKnockout/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OReilleyMVCwKnockout.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        public int AuthorID { get; set; }
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Synopsis { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public virtual Author Author { get; set; }
+    }
+}
