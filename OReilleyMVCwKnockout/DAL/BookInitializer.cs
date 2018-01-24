b200c8072d71bd69b2d50eabdc55a0a97809bc10
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-24 13:32:50 -0600

    Added Author to book initializer

---
+++ OReilleyMVCwKnockout/DAL/BookInitializer.cs
@@ -0,0 +1,27 @@
+﻿using OReilleyMVCwKnockout.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OReilleyMVCwKnockout.DAL
+{
+    public class BookInitializer : DropCreateDatabaseIfModelChanges<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            var author = new Author
+            {
+                Biography = "...",
+                FirstName = "Jamie",
+                LastName = "Munro"
+            };
+
+            var books = new List<Book>
+            {
+
+            };
+        }
+    }
+}
