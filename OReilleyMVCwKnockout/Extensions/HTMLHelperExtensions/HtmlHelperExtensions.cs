dfb8a3c14dc771059fc4459d3f68126d586afa04
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-25 08:24:20 -0600

    Added JSON Serializer

---
+++ OReilleyMVCwKnockout/Extensions/HTMLHelperExtensions/HtmlHelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+namespace OReilleyMVCwKnockout.Extensions.HTMLHelperExtensions
+{
+    public static class HtmlHelperExtensions
+    {
+        ////public static HtmlString HtmlConvertToJson(this HtmlHelperExtensions htmlHelperExtensions, object model)
+        ////{
+        ////    var settings = new JsonSerializerSettings
+        ////    {
+        ////        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+        ////        Formatting = Formatting.Indented
+        ////    };
+
+        ////    return new HtmlString(JsonConvert.SerializeObject(model, settings));
+        //}
+    }
+}
