85fb9a263c07023545daed12a6a76d9e85731d55
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-24 18:13:22 -0600

    Scaffolded Author, initialized Db

--- OReilleyMVCwKnockout/Global.asax.cs
+++ OReilleyMVCwKnockout/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OReilleyMVCwKnockout.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,10 @@ namespace OReilleyMVCwKnockout
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
         }
     }
 }
