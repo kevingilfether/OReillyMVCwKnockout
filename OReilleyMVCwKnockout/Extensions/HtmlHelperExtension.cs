e11557016cd1a3a47c553b3de63e851213a97d31
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-26 13:34:21 -0600

    Fixed HTML helper method

---
+++ OReilleyMVCwKnockout/Extensions/HtmlHelperExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+
+namespace OReilleyMVCwKnockout.Extensions
+{
+    public static class HtmlHelperExtension
+    {
+        public static HtmlString HtmlConvertToJson(this HtmlHelper htmlHelper, object model)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+            return new HtmlString(JsonConvert.SerializeObject(model, settings));
+
+        }
+    }
+}
